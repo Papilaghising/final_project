3345f46d43dd44ae734c0cb5b4b83944053e6dbe
pailaghising <papilaghising1111@gmail.com>
2024-07-12 10:58:10 +0545

    managed nginx configuration

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,6 @@
-data "aws_iam_role" "ssm" {
+data "aws_iam_policy" "ssm" {
     name = "AmazonEC2RoleforSSM"
 }
-data "aws_iam_role" "ecr"{
+data "aws_iam_policy" "ecr"{
     name = "AmazonEC2ContainerRegistryFullAccess"
 }
